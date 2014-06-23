c0af17e327abff0428443e928e14d47b0f110557
Graham Smith <graham@thingie.co.uk>
2014-06-23 12:29:28 +0100

    Nails. Just nails

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -344,14 +344,16 @@ Pathways.LoadScenes = function() {
         //
         var $boats      = $('#india .boats'),
             ratio       = 1050 / 1900,
-            boat_ratio  = 233 / 1900,
+            boat_ratio  = 322 / 1900,
             boat_height = (boat_ratio * window.innerWidth);
 
-        $boats.css({ bottom: 'auto', top: (ratio * window.innerWidth) - boat_height - 40, height: boat_height });
+        // $boats.css({ bottom: 'auto', top: (ratio * window.innerWidth) - boat_height - 40, height: boat_height });
+        $boats.css({ bottom: 0, height: boat_height });
 
         $(window).on('resize', function() {
             boat_height = (boat_ratio * window.innerWidth);
-            $boats.css({ bottom: 'auto', top: (ratio * window.innerWidth) - boat_height - 40, height: boat_height });
+            // $boats.css({ bottom: 'auto', top: (ratio * window.innerWidth) - boat_height - 40, height: boat_height });
+            $boats.css({ height: boat_height });
         })
 
         scenes[idx++] = new ScrollScene({
