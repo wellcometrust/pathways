dec25ce428bf43fa516b3d02c173bd969b8f8abc
Michael Allan <michaelallan@Michaels-MacBook-Pro.local>
2014-06-23 11:46:39 +0100

    Credit this!!!

--- pathways/1-the-mind/credits.php
+++ pathways/1-the-mind/credits.php
@@ -14,36 +14,41 @@ include($docRoot.'/_includes/header.php');
 
         <div class="credits">
 
-            <h3>Wellcome Collection</h3>
+            <div class="inner">
 
-            <ul>
-                <li><span>Author:</span> Mike Jay</li>
-                <li><span>Producer:</span> Jane Audas</li>
-                <li><span>Executive Producer:</span> Danny Birchall</li>
-                <li><span>Project Manager:</span> Oliver Vicars-Harris</li>
-                <li><span>Production Assistant:</span> Joe Simmonds-Issler</li>
-                <li><span>Digital/Production Assistant:</span> Anna Ostrowska</li>
-                <li><span>Content Research:</span> Julia Nurse and Ruth Blue</li>
-                <li><span>Media Production:</span> Chris Chapman</li>
-            </ul>
+                <h1>Credits</h1>
 
-            <h3>Beakus (Air Loom Animation)</h3>
+                <h3>Wellcome Collection</h3>
 
-            <ul>
-                <li><span>Producer:</span> Laura Thomas</li>
-                <li><span>Director:</span> Leo Bridle</li>
-            </ul>
+                <ul>
+                    <li><span>Author:</span> Mike Jay</li>
+                    <li><span>Producer:</span> Jane Audas</li>
+                    <li><span>Executive Producer:</span> Danny Birchall</li>
+                    <li><span>Project Manager:</span> Oliver Vicars-Harris</li>
+                    <li><span>Production Assistant:</span> Joe Simmonds-Issler</li>
+                    <li><span>Digital/Production Assistant:</span> Anna Ostrowska</li>
+                    <li><span>Content Research:</span> Julia Nurse and Ruth Blue</li>
+                    <li><span>Media Production:</span> Chris Chapman</li>
+                </ul>
 
-            <h3>Clearleft</h3>
+                <h3>Clearleft</h3>
 
-            <ul>
-                <li><span>Designer:</span> Michael Allan</li>
-                <li><span>Developer:</span> Graham Smith</li>
-                <li><span>UX Designer:</span> Richard Rutter</li>
-                <li><span>Project Manager:</span> Jessica Jebari</li>
-            </ul>
+                <ul>
+                    <li><span>Designer:</span> Michael Allan</li>
+                    <li><span>Developer:</span> Graham Smith</li>
+                    <li><span>UX Designer:</span> Richard Rutter and Ben Sauer</li>
+                    <li><span>Project Manager:</span> Jessica Jebari</li>
+                </ul>
 
-        </div>
+                <h3>Beakus <span>(Air Loom Animation)</span></h3>
+
+                <ul>
+                    <li><span>Producer:</span> Laura Thomas</li>
+                    <li><span>Director:</span> Leo Bridle</li>
+                </ul>
+
+            </div><!-- .inner -->
+        </div><!-- .credits -->
 
 
     </main>
