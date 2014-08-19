8237f94ad93214919d4af84f8e8555359c792758
Jeremy Keith <jeremy@adactio.com>
2014-08-19 13:12:03 +0100

    Kahn gallery for The Obscene Doctor

--- pathways/2-the-collectors/5-obscene-doctor/_panels/2-kahns-anatomical-museum.php
+++ pathways/2-the-collectors/5-obscene-doctor/_panels/2-kahns-anatomical-museum.php
@@ -1,5 +1,5 @@
 
-<div id="kahns-anatomical-museum" class="panel" data-config='{ "offset_height": 150, "background": { "type": "image", "preserve_ratio": true } }'>
+<div id="kahns-anatomical-museum" class="panel" data-config='{ "offset_height": 2400, "background": { "type": "image", "preserve_ratio": true } }'>
 
     <div class="bg-container">
         <img alt="" src="/_assets/img/spacer.png">
@@ -11,45 +11,104 @@
 
             <div class="obscene-panels" data-component="modal">
 
-                <div class="text-panel">Like Rackstrows before it, visitors to self-styled ‘doctor’ Joseph Kahn’s anatomical museum on Oxford Street encountered a life-size anatomical Venus and shocking models of the pathology of venereal disease. They could view microscopic slides and listen to daily lectures about the ‘wondrous’ structure and function of the human body.</div>
+                <div class="text-panel">
+                    <p>Like Rackstrows before it, visitors to self-styled ‘doctor’ Joseph Kahn’s anatomical museum on Oxford Street encountered a life-size anatomical Venus and shocking models of the pathology of venereal disease. They could view microscopic slides and listen to daily lectures about the ‘wondrous’ structure and function of the human body.</p>
+                </div>
                 <div class="image-panel">
-                    <svg class="info-box modal" data-image="" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/1.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/1-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
                         <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
                         <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
                     </svg>
                 </div>
                 <div class="image-panel">
-                    <svg class="info-box modal" data-image="" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/2.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/2-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
                         <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
                         <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
                     </svg>
                 </div>
                 <div class="image-panel">
-                    <svg class="info-box modal" data-image="" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/3.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/3-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
                         <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
                         <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
                     </svg>
                 </div>
                 <div class="image-panel">
-                    <svg class="info-box modal" data-image="" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/4.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/4-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
                         <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
                         <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
                     </svg>
                 </div>
                 <div class="image-panel">
-                    <svg class="info-box modal" data-image="" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/5.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/5-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
                         <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
                         <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
                     </svg>
                 </div>
                 <div class="image-panel">
-                    <svg class="info-box modal" data-image="" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/6.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/6-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                        <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
+                        <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
+                    </svg>
+                </div>
+                <div class="image-panel">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/7.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/7-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                        <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
+                        <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
+                    </svg>
+                </div>
+                <div class="image-panel">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/8.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/8-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                        <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
+                        <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
+                    </svg>
+                </div>
+                <div class="image-panel">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/9.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/9-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                        <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
+                        <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
+                    </svg>
+                </div>
+                <div class="image-panel">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/10.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/10-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                        <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
+                        <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
+                    </svg>
+                </div>
+                <div class="image-panel">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/11.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/11-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                        <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
+                        <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
+                    </svg>
+                </div>
+                <div class="image-panel">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/12.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/12-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
+                        <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
+                        <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
+                    </svg>
+                </div>
+                <div class="image-panel">
+                    <img alt="" src="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/13.jpg">
+                    <svg class="info-box modal" data-image="/pathways/2-the-collectors/_assets/5-obscene-doctor/kahn/full/13-large.jpg" version="1.1" xmlns="http://www.w3.org/2000/svg" style="width: 70px; height: 70px;">
                         <circle class="outer" cx="50%" cy="50%" r="30" fill="rgb(92,184,178)"></circle>
                         <circle class="inner" cx="50%" cy="50%" r="20" fill="#fff"></circle>
                     </svg>
                 </div>
 
-                <div class="text-panel">Ladies were omitted only on specific days, when Kahn insisted the most offensive models were removed from display.</div>
+                <div class="text-panel">
+                    <p>Ladies were admitted only on specific days, when Kahn insisted the most offensive models were removed from display.</p>
+                </div>
             </div>
             
         </div>                    
