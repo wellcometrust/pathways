bd9f2111f83f07a7813768e2420a4099ddfc2f6e
Danielle Huntrods <daniellehuntrods@gmail.com>
2014-10-27 14:41:07 +0000

    Survey text modification

--- patterns/survey.php
+++ patterns/survey.php
@@ -1,5 +1,5 @@
 <div class="survey">
 
-    <a href="http://survey.euro.confirmit.com/wix/p1839093604.aspx">Please tell us about your experience</a>
+    <a href="http://survey.euro.confirmit.com/wix/p1839093604.aspx">Please help us by telling us what you thought of this digital story</a>
 
 </div>
