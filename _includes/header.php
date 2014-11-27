778344beb4561c595f177835b15bbd66ab92744f
Danielle Huntrods <daniellehuntrods@gmail.com>
2014-11-27 17:30:23 +0000

    Updating typekit font references

--- _includes/header.php
+++ _includes/header.php
@@ -7,7 +7,6 @@
     <meta name="description" content="<?= $page->getDescription()?>">
     <meta name="viewport" content="initial-scale=1,width=device-width">
 
-    <link rel="stylesheet" href="//f.fontdeck.com/s/css/VWVFQa58PbJBMqY5yx0LaLm3Wok/<?= $_SERVER['SERVER_NAME'] ?>/44200.css" type="text/css">
     <link rel="stylesheet" type="text/css" href="<?= $page->getAsset('build/css/main.css') ?>">
 
     <script src="/_assets/js/lib/modernizr-2.8.3.custom.min.js"></script>
@@ -22,7 +21,7 @@
 
 	</script>
 
-    <script src="//use.typekit.net/fhj6ovs.js"></script>
+    <script src="//use.typekit.net/nlx6tdo.js"></script>
     <script>try{Typekit.load();}catch(e){}</script>
 
 </head>
