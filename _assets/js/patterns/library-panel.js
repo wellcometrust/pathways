f771f63181906571f45af1ed6f84832a905a0cf7
Danielle Huntrods <daniellehuntrods@gmail.com>
2014-12-01 11:00:01 +0000

    Implement toggle library panel GA open/close

--- _assets/js/patterns/library-panel.js
+++ _assets/js/patterns/library-panel.js
@@ -1,4 +1,8 @@
 Pathways.components.libraryPanel = function(element, data) {
+    var reO = /l2 share open/g,
+        reC = /l2 share close/g,
+        repO = 'l2 share open',
+        repC = 'l2 share close';
 
     function closePanel($panel) {
         $panel.css('transform', 'translate(' + ($panel.outerWidth()) + 'px, ' + ($panel.outerHeight() - 60) + 'px)');
@@ -10,17 +14,27 @@ Pathways.components.libraryPanel = function(element, data) {
         $panel.addClass('active');
     }
 
+    function toggleActiveGA($el, str, re1, re2) {
+        var newstr2 = str.replace(re1, re2);
+        $el.data('ga', newstr2);
+    }
+
     $(element).on('click', '.handle', function() {
-        var $panel = $(this).parent();
+        var $this = $(this);
+        var $panel = $this.parent();
+        var gaData = $this.data('ga');
+        //console.log(gaData);
 
         if ($panel.hasClass('active')) {
             closePanel($panel);
+            toggleActiveGA($this, gaData, reC, repO);
+
         } else {
             openPanel($panel);
-
             $(window).one('scroll', function() {
                 closePanel($panel);
             });
+            toggleActiveGA($this, gaData, reO, repC);
         }
     });
 };
