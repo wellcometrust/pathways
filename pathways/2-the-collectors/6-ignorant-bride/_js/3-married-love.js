35dbca590463798b8e5750cd5a3c495195a2b193
Danielle Huntrods <daniellehuntrods@gmail.com>
2014-12-10 12:19:27 +0000

    Content for Marie Stopes gallery

--- pathways/2-the-collectors/6-ignorant-bride/_js/3-married-love.js
+++ pathways/2-the-collectors/6-ignorant-bride/_js/3-married-love.js
@@ -1,20 +1,27 @@
 Pathways.components.gallery.madeAFilm = {
     data: {
-        location: 'galleries/tools-of-mesmerism/',
+        location: 'galleries/they-even-made-a-film/',
         images: [{
-            image: 'V0016530',
+            image: '01_Maisie_Dick_in_love',
+            text: 'Maisie’s Marriage was produced in the summer of 1923. It follows the story of Maisie Burrows, the eldest of ten children, and her beau, fireman Dick Reading.'
         }, {
-            image: 'L0023349',
+            image: '02_Drudgery',
+            text: 'Despite being very much in love with Dick, Maisie refuses his marriage proposal; she doesn’t want a life of drudgery, with too many children and not enough money.'
         }, {
-            image: 'L0023350',
+            image: '03_Jump',
+            text: 'Maisie is thrown out of her home and attempts suicide. She ends up as a maid in the household of a happily married couple who have three children.'
         }, {
-            image: 'L0023351',
+            image: '04_wedding',
+            text: 'When her new home catches fire, Maisie is rescued by Dick and finally agrees to marry him.'
         }, {
-            image: 'L0023352',
+            image: '05_credits',
+            text: 'The film was promoted as being written by Marie Stopes, though it was actually authored by her co-writer Walter Summers.'
         }, {
-            image: 'M0006352',
+            image: '06_Marie Stopes',
+            text: 'The association with Stopes – along with the original title of Married Love – was guaranteed to generate a lot of publicity. Five years after she had published her first book, Married Love was still selling hundreds of thousands of copies a year, and Stopes had also written two other successful books.'
         }, {
-            image: 'V0011096',
+            image: '07_Film_poster_crop',
+            text: 'The Stopes name also caused problems. While a reviewer described the film as nothing more than ‘a straightforward human story of sentimental rather than sexual appeal’, the British Board of Film Censors was concerned about ‘the title, taken in conjunction with the name of the book and the authoress referred to’. It felt the production was ‘propaganda on a subject unsuitable for discussion in a Cinema Theatre.'
         }, ]
     }
 };
