48fbe554b51822f5d9e21dafd2ffb7cade95f78e
Danielle Huntrods <daniellehuntrods@gmail.com>
2014-12-10 15:29:16 +0000

    Adding obscene gallery captions

--- pathways/2-the-collectors/5-obscene-doctor/_js/3-quackery.js
+++ pathways/2-the-collectors/5-obscene-doctor/_js/3-quackery.js
@@ -3,24 +3,34 @@ Pathways.components.gallery.quacksOfThe18thCentury = {
         location: 'galleries/quacks-of-the-18th-century/',
         images: [{
             image: 'L0018661a',
+            text: 'Quacks and travelling medicine vendors were a common sight in the 17th and 18th century.'
         }, {
             image: 'M0013726',
+            text: 'They were selling snake oil but sometimes antidotes to snake poison, like this vendor.'
         }, {
             image: 'V0007356a',
+            text: 'Some of them were women: Anne Manning, a quack doctor, outside her cottage with Betty Upton.'
         }, {
             image: 'V0010929a',
+            text: 'Doctor Kill’em-or-Cure’em irresponsibly dispensing his potions.'
         }, {
             image: 'V0011005',
+            text: 'A sailor with a bandaged eye consulting a quack doctor.'
         }, {
             image: 'V0016170',
+            text: 'Doctor Bossy selling his wares on stage with assistants at Covent Garden, London.'
         }, {
             image: 'V0016171',
+            text: 'Doctor Botherum, perhaps based on Doctor Bossy, selling his goods to a raucous crowd.'
         }, {
             image: 'V0016188a',
+            text: 'Doctor Rock using his horse-drawn carriage as a stage to present his remedies to a crowd.'
         }, {
             image: 'V0016215',
+            text: 'This vendor puts on a real show, assisted by an elaborately dressed man and an owl.'
         }, {
             image: 'V0016230a',
+            text: 'The colourful quack troupe including a donkey, a monkey and a fool blowing a trumpet.'
         }]
     }
 };
--- pathways/2-the-collectors/5-obscene-doctor/_js/4-obscene-publications-act.js
+++ pathways/2-the-collectors/5-obscene-doctor/_js/4-obscene-publications-act.js
@@ -3,24 +3,34 @@ Pathways.components.gallery.indecentSexualImages = {
         location: 'galleries/indecent-sexual-images/',
         images: [{
             image: 'L0038201',
+            text: 'Coloured illustration of scrotum diseased with Syphilis.'
         }, {
             image: 'L0038202',
+            text: 'Coloured illustration of labia diseased with Syphilis ‘Sclerosis of the Right Labium Majus’.'
         }, {
             image: 'L0038205',
+            text: 'Skin of the scrotum and perineum area diseased with Syphilis.'
         }, {
             image: 'L0038206',
+            text: 'Female breast diseased with Syphilis.'
         }, {
             image: 'L0038209',
+            text: 'Male pubis and penis diseased with Syphilis. Paraphimosis from Venereal Ulcer on the Foreskin. Inflammatory Edema. Suppurative Adenitis in both groins.'
         }, {
             image: 'L0060656',
+            text: 'Syphilitic frambesia (‘raspberry’).'
         }, {
             image: 'L0074250',
+            text: 'Combycomata, female perineum.'
         }, {
             image: 'V0009962',
+            text: 'Female genitalia showing severely diseased tissue and hypertrophy of the clitoris.'
         }, {
             image: 'V0009971',
+            text: 'Female genitalia showing severely diseased tissue caused by syphilis: extensive sores and abcesses are seen extending up the abdomen and torso.'
         }, {
             image: 'V0010253',
+            text: 'A section of leg with a swelling on the shin below the knee; and male genitalia with a lump on the testicles.'
         }]
     }
 };
