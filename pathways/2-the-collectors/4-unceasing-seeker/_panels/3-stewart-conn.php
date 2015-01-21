59f6925950e4d2c8c0b69cf3c567d00cefc08e18
Danielle Huntrods <daniellehuntrods@gmail.com>
2015-01-21 10:31:15 +0000

    Stewart Conn poem line breaks

--- pathways/2-the-collectors/4-unceasing-seeker/_panels/3-stewart-conn.php
+++ pathways/2-the-collectors/4-unceasing-seeker/_panels/3-stewart-conn.php
@@ -18,28 +18,28 @@
                 <i>My plans exist in my mind like a jigsaw puzzle… and gradually I shall be able to piece it together</i>
                 <p>Sir Henry Wellcome, 1853-1936</p>
             </blockquote>
-            <p>As though a neolithic arrowhead he’d unearthed
-            at the age of four had entered his bloodstream,
-            its sliver of flint sparking an obsession, the items
-            he acquired over the years ranged from Darwin’s
-            whalebone walking-stick, Napoleon’s toothbrush
-            and a pair of Florence Nightingale’s mocassins
+            <p>As though a neolithic arrowhead he’d unearthed<br>
+            at the age of four had entered his bloodstream,<br>
+            its sliver of flint sparking an obsession, the items<br>
+            he acquired over the years ranged from Darwin’s<br>
+            whalebone walking-stick, Napoleon’s toothbrush<br>
+            and a pair of Florence Nightingale’s mocassins<br>
             to shrunken heads and tons of ancient armour.</p>
 
-            <p>But despite all his squirrelling, the museum
-            to house them remained illusory. Picture him,
-            his explorer’s garb and trappings laid aside,
-            increasingly hemmed in, until overwhelmed
-            by the mouldering mountain, moth-eaten,
-            worm-ridden, filling his Willesden warehouse
-            to overflowing, he ended up part haunted,
-            part devoured by more than enough to fill five
+            <p>But despite all his squirrelling, the museum<br>
+            to house them remained illusory. Picture him,<br>
+            his explorer’s garb and trappings laid aside,<br>
+            increasingly hemmed in, until overwhelmed<br>
+            by the mouldering mountain, moth-eaten,<br>
+            worm-ridden, filling his Willesden warehouse<br>
+            to overflowing, he ended up part haunted,<br>
+            part devoured by more than enough to fill five<br>
             Louvres but not some insatiable chasm within.</p>
 
-            <p>Garlanded and afforded the scholastic kudos
-            he craved, a vast fortune securely amassed,
-            did he at the last find respite from cupidity
-            or in a lonely hall of mirrors cower from those
+            <p>Garlanded and afforded the scholastic kudos<br>
+            he craved, a vast fortune securely amassed,<br>
+            did he at the last find respite from cupidity<br>
+            or in a lonely hall of mirrors cower from those<br>
             behemoths hemming him in, their foetid breath?</p>
 
 
